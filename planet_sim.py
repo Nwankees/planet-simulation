888cf7fe70f40f58e10e00b8b7c167bdf86ce492
Emmanuel Nwankwo <eenwankwo12@gmail.com>
2025-11-11 11:44:56 -0500

    Add Planet class

--- planet_sim.py
+++ planet_sim.py
@@ -1,4 +1,51 @@
 import pygame
 import math
-
 pygame.init()
+
+WIDTH, HEIGHT = 800, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Planet Simulation")
+
+WHITE = (255, 255, 255)
+
+class Planet:
+    AU = 149.6E6 * 1000 # km -> m
+    G = 6.67428e-11
+    SCALE = 250 / AU # 1AU = 100 pixels
+    TIMESTEP = 3600 * 24
+
+    def __init__(self, x, y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+
+        self.orbit = []
+        self.sun = False
+        self.distance_to_sun = 0
+
+        self.x_vel = 0
+        self.y_vel = 0
+
+    def draw(self, win):
+        x = self.x * self.SCALE + WIDTH / 2
+        y = self.y * self.SCALE + HEIGHT / 2
+        pygame.draw.circle(win, self.color, (x,y), self.radius)
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+       #WIN.fill(WHITE)
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+main()
